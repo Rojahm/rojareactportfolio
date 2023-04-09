0aef7f37b6bcee2db9710f7f7159544aa64a234e
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-09 22:25:12 +0330

    menu fixed on top

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,14 @@
 import "./Menu.css";
 const Menu = ({ menuItems }) => {
   return (
-    <div className="Menu d-flex justify-content-evenly">
-      {menuItems.map((item, i) => (
-        <a href="/" key={i}>
-          {item.slice(0, 1).toUpperCase() + item.slice(1)}
-        </a>
-      ))}
+    <div className="Menu">
+      <div className="bar">
+        {menuItems.map((item, i) => (
+          <a href="/" key={i}>
+            {item.slice(0, 1).toUpperCase() + item.slice(1)}
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
