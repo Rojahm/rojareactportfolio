9a26f66e7318f0286dcdc45c93d16618b6986947
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-11 19:49:57 +0330

    api done not rendered

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,16 +2,20 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
-const Carousel = () => {
-  return (
-    <>
-      <OwlCarousel className="owl-theme" loop margin={10} nav center>
-        <div className="item">
-          <h4>1</h4>
-        </div>
-      </OwlCarousel>
-    </>
-  );
+const Carousel = ({ sites, ready }) => {
+  if (ready) {
+    return (
+      <>
+        <OwlCarousel className="owl-theme" loop margin={10} nav center>
+          <div className="item">
+            <h4>1{sites[0].name}</h4>
+          </div>
+        </OwlCarousel>
+      </>
+    );
+  } else {
+    return <p>Loadin</p>;
+  }
 };
 
 export default Carousel;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
-// import { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Carousel from "./Carousel";
 
 const Projects = () => {
-  // const [sites, setSites] = useState({});
-  // const [ready, setReady] = useState("false");
+  const [sites, setSites] = useState({});
+  const [ready, setReady] = useState(false);
   // const [error, setError] = useState();
   const tok = "HIzdHiGenwgtOATwfhAgmAlDfMaV7psTwMJmnf6w_6I";
   const url = "https://api.netlify.com/api/v1/sites";
@@ -15,18 +15,22 @@ const Projects = () => {
           Authorization: `Bearer ${tok}`,
         },
       })
-      .then((response) => console.log(response));
+      .then((response) => {
+        setReady(true);
+        console.log(response.data);
+        setSites(response.data);
+      });
     // .catch((error) => setError(error));
   };
 
-  // if (!ready) {
-  getSites();
-  // }
+  if (!ready) {
+    getSites();
+  }
 
   return (
     <div className="Projects">
       <p>from projects</p>
-      <Carousel />
+      <Carousel sites={sites} ready={ready} />
     </div>
   );
 };
