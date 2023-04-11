5cf25b95329e3ab931d68bee0ab78a91c93b60df
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-12 01:28:30 +0330

    added sites pics and url links

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,15 +3,44 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const Carousel = ({ sites, ready }) => {
+  const options = {
+    loop: true,
+    center: true,
+    nav: true,
+    margin: 10,
+    responsiveClass: true,
+    resposive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      900: {
+        items: 5,
+      },
+    },
+  };
   if (ready) {
     return (
-      <>
-        <OwlCarousel className="owl-theme" loop margin={10} nav center>
-          <div className="item">
-            <h4>1{sites[0].name}</h4>
-          </div>
+      <div className="Carousel">
+        <OwlCarousel className="owl-theme" {...options}>
+          {sites.map((site) => {
+            return (
+              <div className="item mx-3 rounded">
+                <a href={site.ssl_url}>
+                  <img
+                    className="mb-3"
+                    src={site.screenshot_url}
+                    alt={site.name}
+                  />
+                </a>
+                <h4>{site.name.split("-").slice(0, 2).join(" ") + "!"}</h4>
+              </div>
+            );
+          })}
         </OwlCarousel>
-      </>
+      </div>
     );
   } else {
     return <p>Loadin</p>;
