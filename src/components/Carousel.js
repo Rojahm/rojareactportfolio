7323c49b802a07f3aa4f9fe72f40158de7567934
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-11 17:11:30 +0330

    render

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,13 +2,15 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
-const Carousel = () => {
+const Carousel = ({ sites }) => {
   return (
     <>
       <OwlCarousel className="owl-theme" loop margin={10} nav center>
-        <div className="item">
-          <h4>1</h4>
-        </div>
+        {sites.map((site, i) => (
+          <div className="item">
+            <h4 key={i}>{site.name}</h4>
+          </div>
+        ))}
       </OwlCarousel>
     </>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,12 @@
+import { useState } from "react";
 import axios from "axios";
 import Carousel from "./Carousel";
 
 const Projects = () => {
-  // const redirectUrl = "https://sparkly-mermaid-d9b96d.netlify.app/";
+  const [sites, setSites] = useState({});
+  // const [error, setError] = useState();
   const tok = "HIzdHiGenwgtOATwfhAgmAlDfMaV7psTwMJmnf6w_6I";
-  // const clientId = "bnjmMi_LH_C98kBnMB38oMeJnYsuNiztNTnW-FNhIZA";
-  // const clientScrt = "YvwrBrdjYXcNz5YIsTR7E4Ba7Q9NaJGoJ3Ah7Rbi7m8";
   const url = "https://api.netlify.com/api/v1/sites";
-  // const oath =
-  //   "https://app.netlify.com/authorize?client_id=bnjmMi_LH_C98kBnMB38oMeJnYsuNiztNTnW-FNhIZA&response_type=token&redirect_uri=https://sparkly-mermaid-d9b96d.netlify.app/&state=";
   const getSites = () => {
     axios
       .get(url, {
@@ -16,14 +14,14 @@ const Projects = () => {
           Authorization: `Bearer ${tok}`,
         },
       })
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+      .then((response) => setSites(response.data));
+    // .catch((error) => setError(error));
   };
   getSites();
   return (
     <div className="Projects">
       <p>from projects</p>
-      <Carousel />
+      <Carousel sites={sites} />
     </div>
   );
 };
