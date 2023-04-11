2a2224c2dce0b57bb357bfaaedabd1dc7d02adfe
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-11 18:46:27 +0330

    api

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 import axios from "axios";
 import Carousel from "./Carousel";
 
 const Projects = () => {
-  const [sites, setSites] = useState({});
-  const [ready, setReady] = useState("false");
+  // const [sites, setSites] = useState({});
+  // const [ready, setReady] = useState("false");
   // const [error, setError] = useState();
   const tok = "HIzdHiGenwgtOATwfhAgmAlDfMaV7psTwMJmnf6w_6I";
   const url = "https://api.netlify.com/api/v1/sites";
@@ -15,22 +15,18 @@ const Projects = () => {
           Authorization: `Bearer ${tok}`,
         },
       })
-      .then((response) => {
-        console.log(response);
-        setSites(response.data);
-        setReady(true);
-      });
+      .then((response) => console.log(response));
     // .catch((error) => setError(error));
   };
 
-  if (!ready) {
-    getSites();
-  }
+  // if (!ready) {
+  getSites();
+  // }
 
   return (
     <div className="Projects">
       <p>from projects</p>
-      <Carousel sites={sites} />
+      <Carousel />
     </div>
   );
 };
