9b037959fb7455b17951a7d5224bdf17b861a209
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-11 16:35:43 +0330

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import Carousel from "./Carousel";
 
 const Projects = () => {
-  const redirectUrl = "https://sparkly-mermaid-d9b96d.netlify.app/";
+  // const redirectUrl = "https://sparkly-mermaid-d9b96d.netlify.app/";
   const tok = "HIzdHiGenwgtOATwfhAgmAlDfMaV7psTwMJmnf6w_6I";
-  const clientId = "bnjmMi_LH_C98kBnMB38oMeJnYsuNiztNTnW-FNhIZA";
-  const clientScrt = "YvwrBrdjYXcNz5YIsTR7E4Ba7Q9NaJGoJ3Ah7Rbi7m8";
+  // const clientId = "bnjmMi_LH_C98kBnMB38oMeJnYsuNiztNTnW-FNhIZA";
+  // const clientScrt = "YvwrBrdjYXcNz5YIsTR7E4Ba7Q9NaJGoJ3Ah7Rbi7m8";
   const url = "https://api.netlify.com/api/v1/sites";
-  const oath =
-    "https://app.netlify.com/authorize?client_id=bnjmMi_LH_C98kBnMB38oMeJnYsuNiztNTnW-FNhIZA&response_type=token&redirect_uri=https://sparkly-mermaid-d9b96d.netlify.app/&state=";
+  // const oath =
+  //   "https://app.netlify.com/authorize?client_id=bnjmMi_LH_C98kBnMB38oMeJnYsuNiztNTnW-FNhIZA&response_type=token&redirect_uri=https://sparkly-mermaid-d9b96d.netlify.app/&state=";
   const getSites = () => {
     axios
       .get(url, {
