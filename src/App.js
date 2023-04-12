610326caa451a241b9f266a1341656e907af8e33
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-12 21:45:04 +0330

    added footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Cats from "./components/Cats";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   const menuItems = ["roja", "about", "work", "cats", "contact"];
@@ -16,12 +17,19 @@ function App() {
   };
   return (
     <div className="App">
-      <Menu menuItems={menuItems} handleMenu={handleMenu} />
-      {selectedMenu === 0 && <Hero />}
-      {selectedMenu === 1 && <About />}
-      {selectedMenu === 2 && <Projects />}
-      {selectedMenu === 3 && <Cats />}
-      {selectedMenu === 4 && <Contact />}
+      <div className="header">
+        <Menu menuItems={menuItems} handleMenu={handleMenu} />
+      </div>
+      <div className="content">
+        {selectedMenu === 0 && <Hero />}
+        {selectedMenu === 1 && <About />}
+        {selectedMenu === 2 && <Projects />}
+        {selectedMenu === 3 && <Cats />}
+        {selectedMenu === 4 && <Contact />}
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="bar">
+        <button>open-scource coded by Ro?a</button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
