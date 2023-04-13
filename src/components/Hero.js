95873f6cef04d8b47d5b1c2ea5d423d8b019865b
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-13 16:20:27 +0330

    updated content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,11 +9,11 @@ const Hero = () => {
       <div className="container">
         <p>
           Roja Hosseini - EMEA based front-end developer currently i’m building
-          HTML, CSS, JS mixed projects everyday and have fun with it 🤘. I was
-          an I.T. administrator for about 10 years🤓, then it got boring and now
-          I’m studying new I.T and cloud☁ technologies and freelance front-end
-          developer. Swimming 🏊‍♀️ and bicycle riding 🚴‍♀️ are my activities. I also
-          practice Piano 🎹 and study music theory 🎶.
+          HTML, CSS, JS mixed projects everyday and have fun with it 🤘. React
+          mode is ON! I was an I.T. administrator for about 10 years🤓, then it
+          got boring and now I’m studying new I.T and cloud ☁ technologies and
+          freelance front-end developer. Swimming 🏊‍♀️ and bicycle riding 🚴‍♀️ are
+          my activities. I also practice Piano 🎹 and study music theory 🎶.
         </p>
       </div>
     </div>
