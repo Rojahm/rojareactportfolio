dbcf3edd69ac0579c7653981e2fbeae0d170f28e
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-04-14 01:52:23 +0330

    added id and style to menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,19 @@
+import "./Menu.css";
+
 const Menu = ({ menuItems, handleMenu, selectedMenu }) => {
   return (
     <div className="Menu">
-      <div className="bar">
-        {menuItems.map((item, i) => (
-          <button
-            href="/"
-            style={selectedMenu === i ? { opacity: "0.6" } : { opacity: "1" }}
-            key={i}
-            onClick={() => handleMenu(i)}
-          >
-            {item.slice(0, 1).toUpperCase() + item.slice(1)}
-          </button>
-        ))}
-      </div>
+      {menuItems.map((item, i) => (
+        <button
+          style={selectedMenu === i ? { opacity: "0.5" } : { opacity: "1" }}
+          key={i}
+          id={item}
+          onClick={() => handleMenu(i)}
+        >
+          {console.log(item)}
+          {item.slice(0, 1).toUpperCase() + item.slice(1)}
+        </button>
+      ))}
     </div>
   );
 };
